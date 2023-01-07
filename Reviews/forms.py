ce5566e7f716489b537f4f04693b9f3e0ef6ce1f
Kanha Tomar <kanharajput91@gmail.com>
2023-01-07 16:55:33 +0530

    add label, required, max_length, error_messages to form field

--- Reviews/forms.py
+++ Reviews/forms.py
@@ -1,4 +1,12 @@
 from django import forms
 
+
+# One important note , after setting max_length=20 , user not able to write after 20 character 
+# and to see the error we have to manually inspect code and from elements section remove max_length arguement 
+# to see the error message 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField()     # field to show in form
+    # error_messages can be editable
+    user_name = forms.CharField(label="Your Name",max_length=20, required=True, error_messages= {
+        'max_length' : 'Please enter a shorter name',
+        'required': 'This field is required',
+    })     # field to show in form
