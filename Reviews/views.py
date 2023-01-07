4596e656db13cf02e4c8cce2222651eb2e8fd909
Kanha Tomar <kanharajput91@gmail.com>
2023-01-07 10:48:38 +0530

    review app linked with project

--- Reviews/views.py
+++ Reviews/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def review(request):
+    return HttpResponse("Revies app succesfully linked with project")
