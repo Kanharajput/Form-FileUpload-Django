8a95db19c38849c3da1fff125983495e4d3b4160
Kanha Tomar <kanharajput91@gmail.com>
2023-01-07 15:54:51 +0530

    Django forms to render form

--- Reviews/views.py
+++ Reviews/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from .forms import ReviewForm
+
 
 # Create your views here.
 
 def review(request):
     # there are two different request coming to this view so check for POST as POST have the data
-    if request.method == "POST":             
-        entered_name = request.POST['username']         # request have POST as dictionary and name which is html as key entered data by user is the value
+    if request.method == "POST":      
+        form = ReviewForm(request.POST)                 # getting form data from POST method
+        
+        # if the form is valid render thank-you page
+        # django automatically check the form for it's validness
+        if form.is_valid():
+            print(form.cleaned_data)       # cleaned_data is a dictionary having user entered data in key-value pair
+            return HttpResponseRedirect("/thank-you")        # / denote the host url, good practise to redirect rather then directly render thank-you page here
 
-        if entered_name == "":
-            return render(request,'Reviews/review.html',{'has_error':True})
-
-        print(entered_name)
-        return HttpResponseRedirect("/thank-you")        # / denote the host url, good practise to redirect rather then directly render thank-you page here
-
-    return render(request,'Reviews/review.html',{'has_error':False})            # for GET request this review.html is render
+    # below code is for GET request
+    form = ReviewForm()          # initiate the form
+    return render(request,'Reviews/review.html',{'form':form})            # for GET request this review.html is render
 
 
 def thank_you(request):
