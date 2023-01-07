1c29a52fde453e76ae4adea0988c8b35febc7af4
Kanha Tomar <kanharajput91@gmail.com>
2023-01-07 14:18:58 +0530

    handling form submission and extracting data

--- Reviews/urls.py
+++ Reviews/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.review)
+    path("",views.review),                  # url http://127.0.0.1:8000/
+    path("thank-you",views.thank_you),         # url http://127.0.0.1:8000/thank-you
 ]
--- Reviews/views.py
+++ Reviews/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def review(request):
-    return render(request,'Reviews/review.html')
+    # there are two different request coming to this view so check for POST as POST have the data
+    if request.method == "POST":             
+        entered_name = request.POST['username']         # request have POST as dictionary and name which is html as key entered data by user is the value
+        print(entered_name)
+        return HttpResponseRedirect("/thank-you")        # / denote the host url, good practise to redirect rather then directly render thank-you page here
+
+    return render(request,'Reviews/review.html')            # for GET request this review.html is render
+
+
+def thank_you(request):
+    return render(request,"Reviews/thank-you.html")
+
+
+
+
