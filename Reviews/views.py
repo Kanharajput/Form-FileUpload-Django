b27127da3fb1dfdd36163e03fd5e843e8b47c391
Kanha Tomar <kanharajput91@gmail.com>
2023-01-08 15:54:29 +0530

    add base template

--- Reviews/views.py
+++ Reviews/views.py
@@ -24,30 +24,6 @@ class ReviewView(View):
         # if form is not valid render the page with same form instance
         return render(request,'Reviews/review.html',{'form':form})        
 
-'''
-def review(request):
-    # there are two different request coming to this view so check for POST as POST have the data
-    if request.method == "POST":      
-        form = ReviewForm(request.POST)                 # getting form data from POST method
-        
-        # if the form is valid render thank-you page
-        # django automatically check the form for it's validness
-        # nothing needs to change here this is_valid function works for all the field which added right now 
-        # that's the beauty of using django forms also fields data is also saved in cleaned_data dictionary
-        if form.is_valid():
-            # if we are using ModelForm then there is no need to create a Model instance pass the value
-            # we can directly call save on form
-            form.save()                # save the data to the database
-            return HttpResponseRedirect("/thank-you")        # / denote the host url, good practise to redirect rather then directly render thank-you page here
-
-    else: 
-        form = ReviewForm()          # initiate the form
-    
-    # if the is_valid returns false, then it will send the pre entered form with pre-entered values and also the errors
-    # it show an error when the field is empty to check the error open elements from inspect and remove required from user_name field
-    return render(request,'Reviews/review.html',{'form':form})            
-'''
-
 def thank_you(request):
     return render(request,"Reviews/thank-you.html")
 
