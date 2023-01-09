9cf62e59d3dfc8d80588e41edbfb7c55f0c58c2f
Kanha Tomar <kanharajput91@gmail.com>
2023-01-09 14:06:33 +0530

    upload files

--- Profiles/views.py
+++ Profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
@@ -9,4 +10,5 @@ class CreateProfileView(View):
         return render(request,"Profiles/create_profile.html")
 
     def post(self,request):
-        pass
+        print(request.FILES["image_name"])        # file is the dictionary provided by django to access files
+        return HttpResponseRedirect("/thank-you")
